f102d7c5612effe7037c1bec6ad430e3515ba2e5
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-09-13 01:52:17 -0300

    feat: Implement the ServerThread connection, accept connection and receiving message from client

--- pubsub-java-server/src/main/java/com/example/pubsub/Server.java
+++ pubsub-java-server/src/main/java/com/example/pubsub/Server.java
@@ -1,7 +1,46 @@
 package com.example.pubsub;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static class ServerThread implements Runnable {
+        private Socket socket;
+        private ServerSocket serverSocket;
+        private BufferedReader bufferedReader;
+        private InputStreamReader inputStreamReader;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(5000);
+                System.out.println("Pub/Sub server starting!");
+                while (true) { 
+                    socket = serverSocket.accept();
+                    System.out.println("Client connected: " + socket.getLocalAddress().toString());
+
+                    inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                    String message = bufferedReader.readLine();
+                    
+                    while(message != null && !message.equals("/exit")) {
+                        System.out.println("Received: " + message);
+                        message = bufferedReader.readLine();
+                    }
+
+                    System.out.println("Client disconnected: " + socket.getLocalAddress().toString());
+                    socket.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Thread thread = new Thread(new ServerThread());
+        thread.start();
     }
 }
