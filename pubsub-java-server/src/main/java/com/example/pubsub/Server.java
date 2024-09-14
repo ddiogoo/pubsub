09d134e131ac31799b236bae1362f6036261dc9c
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-09-14 01:38:21 -0300

    fix: Fix the sending messages to the server concurrently

--- pubsub-java-server/src/main/java/com/example/pubsub/Server.java
+++ pubsub-java-server/src/main/java/com/example/pubsub/Server.java
@@ -1,46 +1,35 @@
 package com.example.pubsub;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.example.pubsub.handler.ClientHandler;
+
 public class Server {
-    private static class ServerThread implements Runnable {
-        private Socket socket;
-        private ServerSocket serverSocket;
-        private BufferedReader bufferedReader;
-        private InputStreamReader inputStreamReader;
-
-        @Override
-        public void run() {
-            try {
-                serverSocket = new ServerSocket(5000);
-                System.out.println("Pub/Sub server starting!");
-                while (true) { 
-                    socket = serverSocket.accept();
-                    System.out.println("Client connected: " + socket.getLocalAddress().toString());
-
-                    inputStreamReader = new InputStreamReader(socket.getInputStream());
-                    bufferedReader = new BufferedReader(inputStreamReader);
-                    String message = bufferedReader.readLine();
-                    
-                    while(message != null && !message.equals("/exit")) {
-                        System.out.println("Received: " + message);
-                        message = bufferedReader.readLine();
-                    }
-
-                    System.out.println("Client disconnected: " + socket.getLocalAddress().toString());
-                    socket.close();
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(5000);
+            serverSocket.setReuseAddress(true);
+            while(true) {
+                Socket client = serverSocket.accept();
+                System.out.println(
+                    "New client connected: " + 
+                    client.getInetAddress().getHostAddress()
+                );
+                new Thread(new ClientHandler(client)).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            if(serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) { 
+                    throw new RuntimeException(e.getMessage());
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e.getMessage());
             }
         }
-    }
-    public static void main(String[] args) {
-        Thread thread = new Thread(new ServerThread());
-        thread.start();
     }
 }
